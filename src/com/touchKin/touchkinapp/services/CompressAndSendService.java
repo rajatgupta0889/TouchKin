84622fb7cace5a23f7bdaba3c4af23927e6c92dd
praful-rathore <prafullarathore@gmail.com>
2015-07-09 19:04:15 +0530

    progress bar

--- src/com/touchKin/touchkinapp/services/CompressAndSendService.java
+++ src/com/touchKin/touchkinapp/services/CompressAndSendService.java
@@ -1,13 +1,10 @@
 package com.touchKin.touchkinapp.services;
+
 import java.io.File;
-
 import java.io.IOException;
-import java.security.PublicKey;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
@@ -15,7 +12,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ContentBody;
 import org.apache.http.entity.mime.content.FileBody;
@@ -23,8 +19,6 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.CoreProtocolPNames;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -38,8 +32,6 @@ import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
-import android.widget.Toast;
-
 import com.netcompss.ffmpeg4android.GeneralUtils;
 import com.netcompss.ffmpeg4android.Prefs;
 import com.netcompss.loader.LoadJNI;
@@ -55,18 +47,13 @@ public class CompressAndSendService extends Service {
 	Notification.Builder mBuilder;
 	int id = 1;
 	private File videoDirectory = null;
-	String workFolder = null;
-	String videoFolder = null;
-	String vkLogPath = null;
+	String videoFolder = null, vkLogPath = null, workFolder = null;
 	String videoPath = null;
 	private boolean commandValidationFailedFlag = false;
 	Context _act;
 	Intent intent;
-
 	long totalSize = 0;
-
 	String token;
-
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -130,45 +117,19 @@ public class CompressAndSendService extends Service {
 
 	public class TranscdingBackground extends
 			AsyncTask<String, Integer, Integer> {
-
-		// ProgressDialog progressDialog;
-
 		public TranscdingBackground(Context context) {
 			_act = context;
 		}
 
 		@Override
 		protected void onPreExecute() {
-			// progressDialog = new ProgressDialog(_act);
-			// progressDialog
-			// .setMessage("FFmpeg4Android Transcoding in progress...");
-			// progressDialog.show();
-
 			mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 			mBuilder = new Notification.Builder(getApplicationContext());
 			mBuilder.setContentTitle("Touchkin")
 					.setContentText("Wait for a sec")
-					.setSmallIcon(R.drawable.ic_launcher);
-			// Start a lengthy operation in a background thread
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					int incr;
-					// Do the "lengthy" operation 20 times
-					// for (incr = 0; incr <= 100; incr += 5) {
-					// Sets the progress indicator to a max value, the
-					// current completion percentage, and "determinate"
-					// state
-					mBuilder.setProgress(0, 0, true);
-					// Displays the progress bar for the first time.
-					mNotifyManager.notify(id, mBuilder.build());
-					try {
-						// Sleep for 2 seconds
-						Thread.sleep(1 * 1000);
-					} catch (InterruptedException e) {
-					}
-				}
-			}).start();
+					.setSmallIcon(R.drawable.ic_launcher)
+					.setProgress(0, 0, true);
+			mNotifyManager.notify(id, mBuilder.build());
 
 		}
 
@@ -185,8 +146,8 @@ public class CompressAndSendService extends Service {
 			Log.d("value", val);
 
 			String[] commandStr = { "ffmpeg", "-y", "-i", videoPath, "-strict",
-					"experimental", "-r", "24", "-ac", "2", "-ar", "22050",
-					"-b", "1600k", "-preset", "ultrafast", val };
+					"experimental", "-r", "30", "-ac", "2", "-ar", "22050",
+					"-b", "2000k", "-preset", "ultrafast", val };
 			Log.d("cmd", "");
 
 			LoadJNI vk = new LoadJNI();
@@ -215,14 +176,12 @@ public class CompressAndSendService extends Service {
 		@Override
 		protected void onCancelled() {
 			Log.i(Prefs.TAG, "onCancelled");
-			// progressDialog.dismiss();
 			super.onCancelled();
 		}
 
 		@Override
 		protected void onPostExecute(Integer result) {
 			Log.i(Prefs.TAG, "onPostExecute");
-			// progressDialog.dismiss();
 			// Removes the progress bar
 			mBuilder.setContentText("").setProgress(0, 0, false);
 
@@ -237,25 +196,6 @@ public class CompressAndSendService extends Service {
 			} else {
 				rc = GeneralUtils.getReturnCodeFromLog(vkLogPath);
 			}
-			final String status = rc;
-			// ((Activity) _act).runOnUiThread(new Runnable() {
-			//
-			// @Override
-			// public void run() {
-			// // TODO Auto-generated method stub
-			// Toast.makeText(getApplicationContext(), status,
-			// Toast.LENGTH_LONG).show();
-			// if (status.equals("Transcoding Status: Failed")) {
-			// Toast.makeText(
-			// getApplicationContext(),
-			// "Check: " + vkLogPath
-			// + " for more information.",
-			// Toast.LENGTH_LONG).show();
-			//
-			// }
-			//
-			// }
-			// });
 		}
 	}
 
@@ -265,84 +205,30 @@ public class CompressAndSendService extends Service {
 
 	}
 
-	class ImageUploadTask extends AsyncTask<String, String, String> {
+	class ImageUploadTask extends AsyncTask<Void, Integer, String> {
 
 		Context context;
 
 		public ImageUploadTask(Context context) {
-			// TODO Auto-generated constructor stub
 			this.context = context;
 		}
 
-		@SuppressWarnings("deprecation")
 		@Override
-		protected String doInBackground(String... unsued) {
+		protected String doInBackground(Void... unsued) {
 			return uploadFile();
-			// try {
-			// File file = new File(val);
-			//
-			// // this is storage overwritten on each iteration with bytes
-			// AppController.mHttpClient.getParams().setParameter(
-			// CoreProtocolPNames.PROTOCOL_VERSION,
-			// HttpVersion.HTTP_1_1);
-			//
-			// HttpClient httpClient = AppController.mHttpClient;
-			//
-			// HttpPost httpPost = new HttpPost(
-			// "http://54.69.183.186:1340/kinbook/message/add");
-			//
-			// MultipartEntity entity = new MultipartEntity(
-			// HttpMultipartMode.BROWSER_COMPATIBLE);
-			//
-			//
-			//
-			// entity.addPart("shared_with",
-			// new StringBody(SendTouchPreview.getCheckedParentId()));
-			// entity.addPart("message", new StringBody(
-			// SendTouchPreview.sendmessage.getText().toString()));
-			// ContentBody cbFile = new FileBody(
-			// file,
-			// ContentType.create(getMimeType(file.getAbsolutePath())),
-			// file.getName());
-			//
-			// entity.addPart("media", cbFile);
-			//
-			// // entity.addPart("media", fileBody);
-			// Log.d("ParentId", SendTouchPreview.getCheckedParentId()
-			// .toString());
-			//
-			// // entity.addPart("photoCaption", new
-			// // StringBody(caption.getText()
-			// // .toString()));
-			// httpPost.setEntity(entity);
-			// Log.d("HttpPost", httpPost.getEntity() + "");
-			// HttpResponse response = httpClient.execute(httpPost);
-			// HttpEntity resEntity = response.getEntity();
-			//
-			// System.out.println(response.getStatusLine());
-			// // if (resEntity != null) {
-			// // System.out.println(EntityUtils.toString(resEntity));
-			// // }
-			// // if (resEntity != null) {
-			// // resEntity.consumeContent();
-			// // }
-			// return EntityUtils.toString(resEntity);
-			// } catch (Exception e) {
-			// Log.e(e.getClass().getName(), e.getMessage(), e);
-			// return null;
-			// }
 		}
+
 		@SuppressWarnings("deprecation")
 		private String uploadFile() {
 			String responseString = null;
 
 			HttpClient httpclient = new DefaultHttpClient();
 			AppController.mHttpClient.getParams().setParameter(
-					CoreProtocolPNames.PROTOCOL_VERSION,
-					HttpVersion.HTTP_1_1);
+					CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
 
 			HttpClient httpClient = AppController.mHttpClient;
-			HttpPost httppost = new HttpPost("http://54.69.183.186:1340/kinbook/message/add");
+			HttpPost httppost = new HttpPost(
+					"http://54.69.183.186:1340/kinbook/message/add");
 			httppost.setHeader("Authorization", "Bearer " + token);
 			try {
 				AndroidMultiPartEntity entity = new AndroidMultiPartEntity(
@@ -350,7 +236,7 @@ public class CompressAndSendService extends Service {
 
 							@Override
 							public void transferred(long num) {
-								publishProgress(""+(int) ((num / (float) totalSize) * 100));
+								publishProgress((int) ((num / (float) totalSize) * 100));
 							}
 						});
 
@@ -359,26 +245,14 @@ public class CompressAndSendService extends Service {
 						new StringBody(SendTouchPreview.getCheckedParentId()));
 				entity.addPart("message", new StringBody(
 						SendTouchPreview.sendmessage.getText().toString()));
-				ContentBody cbFile = new FileBody(
-						sourceFile,
-						ContentType.create(getMimeType(sourceFile.getAbsolutePath())),
-						sourceFile.getName());
+				ContentBody cbFile = new FileBody(sourceFile,
+						ContentType.create(getMimeType(sourceFile
+								.getAbsolutePath())), sourceFile.getName());
 				entity.addPart("media", cbFile);
 
 				// entity.addPart("media", fileBody);
 				Log.d("ParentId", SendTouchPreview.getCheckedParentId()
 						.toString());
-
-
-//				entity.addPart("media", cbFile);
-//
-//				// Adding file data to http body
-//				entity.addPart("image", new FileBody(sourceFile));
-//
-//				// Extra parameters if you want to pass to server
-//				entity.addPart("website",
-//						new StringBody("www.androidhive.info"));
-//				entity.addPart("email", new StringBody("abc@gmail.com"));
 
 				totalSize = entity.getContentLength();
 				httppost.setEntity(entity);
@@ -407,56 +281,41 @@ public class CompressAndSendService extends Service {
 		}
 
 		@Override
-		protected void onProgressUpdate(String... unsued) {
+		protected void onProgressUpdate(Integer... unsued) {
+
+			if (unsued[0] != 0 && unsued[0] < 100) {
+
+				mBuilder.setProgress(100, unsued[0], false).setContentText("Sending your touch "+
+						String.valueOf(unsued[0])+"% complete");
+				Log.i(Prefs.TAG, "setting progress notification: " + unsued[0]);
+				try {
+					mNotifyManager.notify(id, mBuilder.build());
+				} catch (Exception e) {
+					Log.i(Prefs.TAG, "Android 2.3 or below? " + e.getMessage());
+				}
+			} else if (unsued[0] == 100) {
+				Log.i(Prefs.TAG,
+						"==== progress is 100, exiting Progress update thread");
+
+				mBuilder.setContentText("Sent")
+				// Removes the progress bar
+						.setProgress(0, 0, false);
+				try {
+					mNotifyManager.notify(id, mBuilder.build());
+				} catch (Exception e) {
+					Log.i(Prefs.TAG, "Android 2.3 or below? " + e.getMessage());
+				}
+
+			}
 		}
 
 		@Override
 		protected void onPostExecute(String sResponse) {
-			try {
-				if (sResponse != null) {
-					// hidepDialog();
-					Log.d("Response", sResponse);
-					JSONObject JResponse = new JSONObject(sResponse);
-					Log.d("JSON", JResponse.toString());
-					Toast.makeText(context, "your message sent",
-							Toast.LENGTH_SHORT).show();
-					mBuilder.setContentText("Sent").setProgress(0, 0, false);
-					mNotifyManager.notify(id, mBuilder.build());
-				} else {
-					mBuilder.setContentText("Problem while sending a touch")
-							.setProgress(0, 0, false);
-					mNotifyManager.notify(id, mBuilder.build());
-					Toast.makeText(context, "Problem while sending a touch",
-							Toast.LENGTH_SHORT).show();
-				}
-			} catch (Exception e) {
-				Log.e(e.getClass().getName(), e.getMessage(), e);
-			}
 			stopService(intent);
 		}
 
 		@Override
 		protected void onPreExecute() {
-			// showpDialog();
-			mBuilder.setContentText("sending...");
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					mBuilder.setProgress(0, 0, true);
-					// Displays the progress bar for the first time.
-					mNotifyManager.notify(id, mBuilder.build());
-					// Sleeps the thread, simulating an operation
-					// that takes time
-					try {
-						// Sleep for 5 seconds
-						Thread.sleep(1 * 1000);
-					} catch (InterruptedException e) {
-						// Log.d(TAG, "sleep failure");
-					}
-				}
-			}
-			// Starts the thread by calling the run() method in its Runnable
-			).start();
 		}
 
 	}
